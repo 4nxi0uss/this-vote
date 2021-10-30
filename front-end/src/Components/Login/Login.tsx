715c113595c74b867015277b399a027290c94936
Artur Zaczek <artur200212@interia.pl>
2021-10-31 00:40:45 +0200

    login validation

--- front-end/src/Components/Login/Login.tsx
+++ front-end/src/Components/Login/Login.tsx
@@ -1,27 +1,47 @@
 import { useState } from 'react';
-import type { MouseEvent, ChangeEvent } from 'react';
+import type { /*MouseEvent,*/ ChangeEvent, FormEvent } from 'react';
+
 import './Login.scss'
 
 const Login = () => {
 
     const [toggleRegistry, setToggleRegistry] = useState<boolean>(false);
-    const [email, setEmail] = useState<string>();
-    const [password, setPassword] = useState<string>();
-    const [secondPassword, setSecondPassword] = useState<string>();
-    const [date, setDate] = useState<string>();
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [secondPassword, setSecondPassword] = useState<string>("");
+    const [date, setDate] = useState<string>("");
 
-    console.log(toggleRegistry)
 
+    const validatedEmail = (toVerified: string) => {
+        const atCheck = toVerified.includes("@");
+        const dotCheck = (toVerified.length / 2) < toVerified.lastIndexOf('.');
+        return (atCheck && dotCheck)
+    }
+    const validatePassword = (toVerified: string) => {
+        let isTrueLetter = false;
+        let isTrueNumber = false;
+
+        toVerified.split('').forEach((letter) => { if (letter === letter.toUpperCase() && letter !== '0') isTrueLetter = Boolean(toVerified.includes(letter)) });
+        toVerified.split('').forEach((letter) => { (Boolean(Number(letter)) === true || Number(letter) === 0) ? (isTrueNumber = true) : (isTrueNumber = false) });
+
+        return (isTrueLetter && isTrueNumber)
+    }
     const handleToggleRegistry = () => {
         setToggleRegistry(!toggleRegistry)
     }
 
-    const handleLogin = (e: MouseEvent<HTMLButtonElement>) => {
+    const handleLogin = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (validatedEmail(email) && validatePassword(password)) {
+            console.log('zalogowany')
+        } else {
+            console.log('nie zalogowany')
+        }
     }
 
-    const handleRegistry = (e: MouseEvent<HTMLButtonElement>) => {
+    const handleRegistry = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        validatedEmail(email)
 
     }
 
@@ -43,20 +63,18 @@ const Login = () => {
         setDate(e.target.value.trim())
     }
 
-    console.log(password, email, secondPassword, date)
-
     const TodayDate = new Date();
 
     return (
         <section className='loginSection'>
             <h2 className='loginTitle'>{!toggleRegistry ? "Sing in" : "Register"}</h2>
-            <form className='loginForm'>
-                <input type="email" placeholder='e-mail' onChange={handleEmail} value={email} />
-                <input type="password" placeholder='password' onChange={handlePassword} value={password} />
+            <form onSubmit={toggleRegistry ? handleRegistry : handleLogin} className='loginForm'>
+                <input required type="email" placeholder='e-mail' onChange={handleEmail} value={email} />
+                <input required type="password" placeholder='password' onChange={handlePassword} value={password} />
                 {!toggleRegistry ? null : <input type='password' placeholder='repeat password' onChange={handleSecondPassword} value={secondPassword} />}
                 {!toggleRegistry ? null : <label>Date of birth</label>}
                 {!toggleRegistry ? null : <input type="date" max={`${TodayDate.getFullYear()}-${TodayDate.getMonth() + 1}-${TodayDate.getDate()}`} onChange={handleDate} value={date} />}
-                <button className='loginBtn' onClick={toggleRegistry ? handleRegistry : handleLogin}>{toggleRegistry ? "Register" : "Log in"}</button>
+                <button className='loginBtn' type='submit'>{toggleRegistry ? "Register" : "Log in"}</button>
             </form>
             <p className="toRegistryParagraph">{toggleRegistry ? "If you have account, please " : "If you don't have account, please"} <button className='toRegistryBtn' onClick={handleToggleRegistry}> {!toggleRegistry ? "register now." : "log into it here."} </button></p>
         </section>

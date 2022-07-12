5294ec71035a89c967eebdb6bf2a5e39e3b306c0
Artur Zaczek <artur200212@interia.pl>
2022-07-12 23:33:20 +0200

    added auto re-auth JWT token

--- front-end/src/Redux/Services/PollApi.ts
+++ front-end/src/Redux/Services/PollApi.ts
@@ -1,9 +1,66 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Mutex } from 'async-mutex'
+
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query'
+
+let userIdD = ''
+
+const mutex = new Mutex()
+
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
+    const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:3022/polls/' })
+    // wait until the mutex is available without locking it
+
+    await mutex.waitForUnlock()
+
+    let result = await baseQuery(args, api, extraOptions)
+
+    const previousArgs = args
+    const previousApi = api
+    const previousExtraOptions = extraOptions
+
+    if (result.error && result.error.status === 401) {
+        // checking whether the mutex is locked
+        if (!mutex.isLocked()) {
+            const release = await mutex.acquire()
+
+            try {
+                const refreshResult = await baseQuery(
+                    args = {
+                        url: 'http://localhost:3022/users/refreshToken',
+                        method: 'POST',
+                        credentials: 'include',
+                        body: { userId: userIdD }
+                    },
+                    api,
+                    extraOptions
+                )
+
+                if (refreshResult.data) {
+                    // retry the initial query
+                    result = await baseQuery(previousArgs, previousApi, previousExtraOptions)
+                } else {
+                    console.warn('re auth api else', refreshResult.error)
+                }
+            } catch (err) {
+                console.warn(err)
+            } finally {
+                // release must be called once the mutex should be released again.
+                await release()
+            }
+        } else {
+            // wait until the mutex is available without locking it
+            await mutex.waitForUnlock()
+            result = await baseQuery(args, api, extraOptions)
+        }
+    }
+    return result
+}
 
 // Define a service using a base URL and expected endpoints
 export const pollApi = createApi({
     reducerPath: 'pollApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3022/polls/' }),
+    baseQuery: baseQueryWithReauth,
     tagTypes: ['Poll'],
     endpoints: (builder) => ({
         getAllPolls: builder.query<any, void>({
@@ -14,18 +71,20 @@ export const pollApi = createApi({
                 }),
         }),
         getPolls: builder.query<any, number>({
-            query: (creatorId) => (
-                {
+            query: (creatorId) => {
+                userIdD = `${creatorId}`;
+                return {
                     url: `getPolls/${creatorId}`,
                     method: "GET",
                     credentials: "include",
-                }),
+                }
+            },
             providesTags: ["Poll"],
         }),
         addPoll: builder.mutation<any, any>({
             query: ({ name, question, number, option, userId, token }) => (
                 {
-                    url: "postPolls/",
+                    url: "postPolls",
                     method: "POST",
                     credentials: "include",
                     headers: {
--- front-end/src/Redux/Services/UserApi.ts
+++ front-end/src/Redux/Services/UserApi.ts
@@ -1,20 +1,80 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { infoLoginType, infoUpdate, registerDataType, registerInfo } from '../ReduxTypes/reduxTypes'
+import { Mutex } from 'async-mutex'
+
+import type { infoLoginType, infoUpdate, registerDataType, registerInfo } from '../ReduxTypes/reduxTypes'
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError, } from '@reduxjs/toolkit/query'
+
+let userIdD = ''
+
+const mutex = new Mutex()
+
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
+    const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:3022/users/' })
+    // wait until the mutex is available without locking it
+
+    await mutex.waitForUnlock()
+
+    let result = await baseQuery(args, api, extraOptions)
+
+    const previousArgs = args
+    const previousApi = api
+    const previousExtraOptions = extraOptions
+
+    if (result.error && result.error.status === 401) {
+        // checking whether the mutex is locked
+        if (!mutex.isLocked()) {
+            const release = await mutex.acquire()
+
+            try {
+                const refreshResult = await baseQuery(
+                    args = {
+                        url: '/refreshToken',
+                        method: 'POST',
+                        credentials: 'include',
+                        body: { userId: userIdD }
+                    },
+                    api,
+                    extraOptions
+                )
+
+                if (refreshResult.data) {
+                    // retry the initial query
+                    result = await baseQuery(previousArgs, previousApi, previousExtraOptions)
+                } else {
+                    console.warn('re auth api else', refreshResult.error)
+                }
+            } catch (err) {
+                console.warn(err)
+            } finally {
+                // release must be called once the mutex should be released again.
+                await release()
+            }
+        } else {
+            // wait until the mutex is available without locking it
+            await mutex.waitForUnlock()
+            result = await baseQuery(args, api, extraOptions)
+        }
+    }
+    return result
+}
 
 // Define a service using a base URL and expected endpoints
 export const userApi = createApi({
     reducerPath: 'userApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3022/users/' }),
+    baseQuery: baseQueryWithReauth,
     tagTypes: ['User'],
     endpoints: (builder) => ({
         //userData
         getUserData: builder.query<any, number>(
             {
-                query: (userId) => ({
-                    url: `getUserData/${userId}`,
-                    method: "GET",
-                    credentials: "include",
-                }),
+                query: (userId) => {
+                    userIdD = `${userId}`;
+                    return {
+                        url: `getUserData/${userId}`,
+                        method: "GET",
+                        credentials: "include",
+                    }
+                },
                 providesTags: ["User"],
             }),
         //infoUpdate
@@ -59,20 +119,30 @@ export const userApi = createApi({
                 }),
             }),
         //registerInfo
-        userRegistery: builder.mutation<registerInfo, registerDataType>({
-            query: ({ email, password }) => (
-                {
-                    url: "register",
+        userRegistery: builder.mutation<registerInfo, registerDataType>(
+            {
+                query: ({ email, password }) => (
+                    {
+                        url: "register",
+                        method: 'POST',
+                        body: {
+                            usersEmail: email,
+                            pass: password
+                        },
+                    }),
+            }),
+        userRefreshToken: builder.mutation<any, string>(
+            {
+                query: (userId) => ({
+                    url: "refreshToken",
                     method: 'POST',
-                    body: {
-                        usersEmail: email,
-                        pass: password
-                    },
+                    credentials: 'include',
+                    body: { userId },
                 }),
-        })
+            }),
     }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetUserDataQuery, useUpdateUserInfoMutation, useUserLoginMutation, useUserRegisteryMutation, useUserActiveMutation, useUserLogoutMutation } = userApi
+export const { useGetUserDataQuery, useUpdateUserInfoMutation, useUserLoginMutation, useUserRegisteryMutation, useUserActiveMutation, useUserLogoutMutation, useUserRefreshTokenMutation } = userApi

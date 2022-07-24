3fb45af246ee13da703fd312e626ee1c024ed9b3
Artur Zaczek <artur200212@interia.pl>
2022-07-24 15:38:42 +0200

    added responsivnes

--- front-end/src/Components/Account/Account.tsx
+++ front-end/src/Components/Account/Account.tsx
@@ -16,14 +16,14 @@ const Account = () => {
         fixedCacheKey: "login"
     });
 
-    const [activeUserAccount, { isSuccess, isError: isErrorActiave, error: activateError }] = useUserActiveMutation()
+    // const [activeUserAccount, { isSuccess, isError: isErrorActiave, error: activateError }] = useUserActiveMutation()
 
     const [updateUserInfo, { isError, error }] = useUpdateUserInfoMutation();
 
     const { data: userData, isError: isUserDataError, error: userDataError } = useGetUserDataQuery(!isLoading && dataLogin?.rows[0]?.user_id)
 
     isError && console.warn(error)
-    isErrorActiave && console.warn(activateError)
+    // isErrorActiave && console.warn(activateError)
     isUserDataError && console.warn(userDataError)
 
     const betterDate = (date: string) => date?.slice(0, 10)
@@ -32,7 +32,7 @@ const Account = () => {
     const [surname, setSurname] = useState<string>(userData?.data[0]?.surname);
     const [date, setDate] = useState<string>(betterDate(userData?.data[0]?.date_of_birth));
     const [typeOfAccount, setTypeOfAccount] = useState<number>(userData?.data[0]?.type_of_account);
-    const [accountStatus, setAccountStatus] = useState<number>(userData?.data[0]?.active);
+    // const [accountStatus, setAccountStatus] = useState<number>(userData?.data[0]?.active);
 
     const TodayDate = new Date();
 
@@ -63,19 +63,19 @@ const Account = () => {
         }
     }
 
-    const handleActive = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
+    // const handleActive = (e: MouseEvent<HTMLButtonElement>) => {
+    //     e.preventDefault();
 
-        activeUserAccount(dataLogin?.rows[0].user_id)
-    }
-    isSuccess && console.log("Account activated succefuly.");
+    //     activeUserAccount(dataLogin?.rows[0].user_id)
+    // }
+    // isSuccess && console.log("Account activated succefuly.");
 
     useMemo(() => {
         setName(userData?.data[0]?.name)
         setSurname(userData?.data[0]?.surname)
         setDate(betterDate(userData?.data[0]?.date_of_birth))
-        setAccountStatus(userData?.data[0]?.active)
         setTypeOfAccount(userData?.data[0]?.type_of_account)
+        // setAccountStatus(userData?.data[0]?.active)
         // eslint-disable-next-line
     }, [userData])
 
@@ -84,7 +84,7 @@ const Account = () => {
 
             <h2 className={b('title')}> Hello {name} </h2>
 
-            <button disabled={accountStatus === 1 ? true : false} className={b('activate-btn')} onClick={handleActive}>Activate</button>
+            {/* <button disabled={accountStatus === 1 ? true : false} className={b('activate-btn')} onClick={handleActive}>Activate</button> */}
 
             <form className={b('form')} onSubmit={handleSubmit} method="submit">
 
@@ -100,10 +100,10 @@ const Account = () => {
                 <label >Type of account:</label>
                 <input type="text" readOnly disabled value={userAcountType[typeOfAccount ?? 0]} />
 
-                <label >Account status:</label>
-                <input type="text" readOnly disabled value={userAcountActivated[accountStatus ?? 0]} />
+                {/* <label >Account status:</label>
+                <input type="text" readOnly disabled value={userAcountActivated[accountStatus ?? 0]} /> */}
 
-                <button className={b('submit-btn')} type="submit">Save</button>
+                <button className={b('form__submit-btn')} type="submit">Save</button>
             </form>
         </section>
     )
--- front-end/src/Components/Header/Header.tsx
+++ front-end/src/Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import style from './Header.module.scss'
@@ -16,6 +17,8 @@ export const handleLogoutConfig = (logout: any, userId: string) => {
 
 const Header = () => {
 
+    const [mobileMenu, setMobileMenu] = useState<boolean>(false)
+
     const location = useLocation();
 
     // eslint-disable-next-line
@@ -29,9 +32,30 @@ const Header = () => {
         handleLogoutConfig(logout, dataLogin?.rows[0].user_id)
     }
 
+    const handleMobileMenu = () => {
+        setMobileMenu((state) => state = !state);
+    }
+
     return (
         <header className={b()}>
             <h1 className={b('title')} > <Link to='/' className={b('title__link')}>ThisVote!!</Link></h1>
+            <nav onClick={handleMobileMenu} className={b('nav-mobile')}>
+                {mobileMenu && <div className={b('nav-mobile__menu')}>
+                    <Link to='/' className={b('nav-mobile__menu__link')}>
+                        Home
+                    </Link>
+                    {!isLoading && dataLogin?.login && <Link to='/Account' className={b('nav-mobile__menu__link')}>
+                        Account
+                    </Link>}
+                    {!isLoading && dataLogin?.login && <Link to='/Polls' className={b('nav-mobile__menu__link')}>
+                        Polls
+                    </Link>}
+                    {!isSuccess && !dataLogin?.login ? <Link to='/login' className={b('nav-mobile__menu__link')}>
+                        Login/Registry
+                    </Link> : <button className={b('nav-mobile__menu__link')} onClick={handleLogout}>Logout</button>}
+                </div>}
+            </nav>
+
             <nav className={b('nav')}>
                 <Link to='/' className={b('nav__link', { mark: location.pathname === '/' })}>
                     Home

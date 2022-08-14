9af98ebe385cca393ed9ab013e03d74abef5b6b4
Artur Zaczek <artur200212@interia.pl>
2022-08-14 23:51:26 +0200

    change rout name

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { getUserData, patchUserInfo, postRegisterUser, postLoginUser, postRefreshToken, postLogoutUser, patchUserTypeAccount } = require('../controllers/users');
+const { getUserData, patchUserInfo, postLoginUser, postRefreshToken, postLogoutUser, patchUserTypeAccount, postSignUpUser } = require('../controllers/users');
 
 const { authMiddleware } = require('../Middleware/Middleware');
 
@@ -8,7 +8,7 @@ const userRouter = express.Router();
 userRouter.get('/get-user-data/:id', authMiddleware, getUserData);
 userRouter.patch('/info-update', authMiddleware, patchUserInfo);
 userRouter.patch('/change-account-type', authMiddleware, patchUserTypeAccount);
-userRouter.post('/register', postRegisterUser);
+userRouter.post('/sign-up', postSignUpUser);
 userRouter.post('/refresh-token', postRefreshToken);
 userRouter.post('/login', postLoginUser);
 userRouter.post('/logout', authMiddleware, postLogoutUser);
